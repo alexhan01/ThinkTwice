55c5efce6f9aaab56d48d445c6ea89920fd0657d
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-10-17 16:10:23 -0700

    add in elements

--- chrome/content.js
+++ chrome/content.js
@@ -4,13 +4,28 @@ console.log("content shit ran")
 
 if(document.getElementById("productTitle")){
     console.log(document.getElementById("productTitle").innerHTML)
+    console.log(document.getElementById("price_inside_buybox").innerHTML)
 }
 
+
+
 document.addEventListener('click', function(e) {
-    if (e.target.matches('#add-to-cart-button')) {
-        alert('clicked!');
+    var reason = ""
+    retval = null;
+    if (e.target.matches('#add-to-cart-button') || e.target.matches('#buy-now-button')) {
+        reason = prompt('Why are you purchasing this product?');
     }
+    if (reason){
+        retval = {
+            title: document.getElementById("productTitle").innerHTML,
+            price: document.getElementById("price_inside_buybox").innerHTML,
+            reason: reason 
+        }
+    }
+    return retval
 });
+
+console.log(retval)
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
