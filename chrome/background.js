da7b5bf05042723279f483b11978bb63ddd8a017
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-10-17 19:02:56 -0700

    chaanges

--- chrome/background.js
+++ chrome/background.js
@@ -1,23 +1,34 @@
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create({url: 'index.html'});
+});
+
+
+
+
+
+
+
+
 // import { isProduct } from './utils/isProduct.js';
 
-let user_signed_in = false;
+// let user_signed_in = false;
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message === 'is_user_signed_in') {
-        sendResponse({
-            message: 'success',
-            payload: user_signed_in
-        });
-    } else if (request.message === 'sign_out') {
-        user_signed_in = false;
-        sendResponse({ message: 'success' });
-    } else if (request.message === 'sign_in') {
-        user_signed_in = true;
-        sendResponse({ message: 'success' });
-    }
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     if (request.message === 'is_user_signed_in') {
+//         sendResponse({
+//             message: 'success',
+//             payload: user_signed_in
+//         });
+//     } else if (request.message === 'sign_out') {
+//         user_signed_in = false;
+//         sendResponse({ message: 'success' });
+//     } else if (request.message === 'sign_in') {
+//         user_signed_in = true;
+//         sendResponse({ message: 'success' });
+//     }
 
-    return true;
-});
+//     return true;
+// });
 
 // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 //   chrome.declarativeContent.onPageChanged.addRules([{
