856cc103e3245340d9b113ee71c84191891f62db
alexhan <alexhan0324@gmail.com>
2020-10-18 11:23:11 -0700

    data.html setup

--- chrome/content.js
+++ chrome/content.js
@@ -14,12 +14,16 @@
 //   // Initialize Firebase
 //   firebase.initializeApp(firebaseConfig);
 
+// const fs = require('fs');
+// const path = require('path');
+
 console.log("content shit ran")
 
 if(document.getElementById("productTitle")){
     console.log(document.getElementById("productTitle").innerHTML)
     console.log(document.getElementById("price_inside_buybox").innerHTML)
 }
+
 chrome.storage.sync.get(["purchases"], function(result) {
     console.log(result)
   });
@@ -51,12 +55,29 @@ document.addEventListener('click', function(e) {
         currPurchases.push(onePurchase)
         // console.log("After Push")
         // console.log(currPurchases)
+        // fs.writeFileSync(path.resolve(dirname__, 'data.json'), JSON.stringify(currPurchases));
+        console.log("Saved to Json")
         chrome.storage.sync.set({"purchases": currPurchases}, function() {
             console.log('Value is set to ' + currPurchases);
+            // fs.writeFileSync(path.resolve(dirname__, 'data.json'), JSON.stringify(currPurchases));
+            // console.log("Saved to Json")
+            // save to json
+            var url = chrome.runtime.getURL("data/data.json")
+            console.log(chrome.runtime.getURL("data/data.json"))
           });
     }
     reason = "";
- 
+    
+    // Sends it to server
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("GET", "", true) // api endpoint of server
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState == 4) {
+    //         var resp = JSON.parse(xhr.responseText);
+    //     }
+    // }
+    // xhr.send();
+
     // return retval;
     // var dummyUserId = "Hasan";
     // writeUserData(dummyUserId, retval);
